c5a329a6b6823edbf759ad4beebccbdd0ee3c6b8
kopigreenx <mokhamad.rizkianto@gmail.com>
2019-08-12 15:22:12 +0700

    add router home and about

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+  <div class="container">
+        <div class="nav-wrapper">
+        <Router-link to="/" class="brand-logo">Vue Tutorial</Router-link>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/About">About</router-link></li>
+            <li><router-link to="#">JavaScript</router-link></li>
+        </ul>
+        </div>
+  </div>
+    </nav>
+</template>
